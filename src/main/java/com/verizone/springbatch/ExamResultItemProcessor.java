b65e8c1cbd9d30c512984176548dade32e1d263c
sanjaya <sanjayameher@gmail.com>
2017-11-08 16:41:08 +0530

    Initial commit

---
+++ src/main/java/com/verizone/springbatch/ExamResultItemProcessor.java
@@ -0,0 +1,25 @@
+package com.verizone.springbatch;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.verizone.springbatch.model.ExamResult;
+
+public class ExamResultItemProcessor implements ItemProcessor<ExamResult, ExamResult>{
+
+	
+	@Override
+	public ExamResult process(ExamResult result) throws Exception {
+		System.out.println("Processing result :"+result);
+		
+		/*
+		 * Only return results which are more than 80%
+		 * 
+		 */
+		if(result.getPercentage() < 80){
+			return null;
+		}
+		
+		return result;
+	}
+
+}
---
+++ src/main/java/com/verizone/springbatch/ExamResultRowMapper.java
@@ -0,0 +1,23 @@
+package com.verizone.springbatch;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.joda.time.LocalDate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.verizone.springbatch.model.ExamResult;
+
+public class ExamResultRowMapper implements RowMapper<ExamResult>{
+
+	@Override
+	public ExamResult mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+		ExamResult result = new ExamResult();
+		result.setStudentName(rs.getString("student_name"));
+		result.setDob(new LocalDate(rs.getDate("dob")));
+		result.setPercentage(rs.getDouble("percentage"));
+			
+		return result;
+	} 
+
+}
